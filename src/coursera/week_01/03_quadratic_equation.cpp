95cf10edf75add180ce86f21300c0ae8886c7b47
Artem Pakhomov <setnemo@gmail.com>
2020-09-26 16:18:10 +0300

    week 01 ex01-04

---
+++ src/coursera/week_01/03_quadratic_equation.cpp
@@ -0,0 +1,50 @@
+/*
+ * 03_quadratic_equation.cpp
+ *
+ *  Created on: 2:34:04 PM Sep 26, 2020
+ *      Author: omentes
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int main_03() {
+	int a, b ,c;
+	double descriminant, scrt_result;
+	vector<double> result;
+	cin >> a >> b >> c;
+	descriminant = (b * b) - 4 * a * c;
+	scrt_result = sqrt(descriminant);
+	if (descriminant < 0.0) {
+		return 0;
+	}
+	if (0 == a) {
+		result.push_back((double)(-c/b));
+	} else if (0 == b) {
+		result.push_back((double)(scrt_result / (2 * a)));
+	}
+	else {
+		if (descriminant >= 0) {
+			result.push_back((-b + scrt_result) / (2 * a));
+			if (descriminant != 0) {
+				result.push_back((-b - scrt_result) / (2 * a));
+			}
+		} else {
+			result.push_back(-b / 2 * a);
+		}
+	}
+
+
+	if (!result.empty()) {
+		for (auto i : result) {
+			cout <<	i << ' ' ;
+		}
+	}
+	return 0;
+}
---
+++ src/coursera/week_01/04_second_occurrence.cpp
@@ -0,0 +1,29 @@
+/*
+ * 04_second_occurrence.cpp
+ *
+ *  Created on: 4:08:20 PM Sep 26, 2020
+ *      Author: omentes
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string in;
+	int result;
+	cin >> in;
+	if (cin.fail()) {
+	    cout << "Please use string";
+	    return 1;
+	}
+	result = -2;
+	for (int i = 0; i < in.length(); ++i) {
+		if ('f' == in[i] && -2 == result) {
+			result++;
+		} else if ('f' == in[i] && -1 == result) {
+			result = i;
+		}
+	}
+	cout << result;
+	return 0;
+}
