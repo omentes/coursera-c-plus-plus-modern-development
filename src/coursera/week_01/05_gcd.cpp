4b3588fd2e03c2f08c097a6ce768e9e991c4c4f0
Artem Pakhomov <setnemo@gmail.com>
2020-09-26 19:44:27 +0300

    week 01 ex05

---
+++ src/coursera/week_01/05_gcd.cpp
@@ -0,0 +1,42 @@
+/*
+ * 05_gcd.cpp
+ *
+ *  Created on: 4:42:22 PM Sep 26, 2020
+ *      Author: omentes
+ */
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	int tmp = a;
+
+    if (a == b) {
+        return a;
+    }
+    if (a > b) {
+        a = b;
+        b = tmp;
+    }
+    return gcd(a, b - a);
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (cin.fail()) {
+	    cout << "Please use numbers";
+	    return 1;
+	}
+	while (a != b) {
+        if (a > b) {
+        	// move less number to a varaible
+        	a = a + b;
+        	b = a - b;
+        	a = a - b;
+        }
+        b = b - a;
+	}
+	cout << a;
+	return 0;
+}
