331aa29767d324b3eda71d5362c7ccdbece736c3
Artem Pakhomov <setnemo@gmail.com>
2020-09-26 22:25:33 +0300

    week 01 ex07

---
+++ src/coursera/week_01/17_devision.cpp
@@ -0,0 +1,28 @@
+/*
+ * 17_devision.cpp
+ *
+ *  Created on: 9:49:15 PM Sep 26, 2020
+ *      Author: omentes
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+
+using namespace std;
+
+int main_17() {
+	int a, b;
+	cin >> a >> b;
+	if (cin.fail()) {
+	    cout << "Please use numbers";
+	    return 1;
+	}
+	if (a < 0 || b < 0 || a > 1000000 || b > 1000000 || 0 == b) {
+		cout << "Impossible";
+	} else {
+		cout << a / b << endl;
+	}
+	return 0;
+}
