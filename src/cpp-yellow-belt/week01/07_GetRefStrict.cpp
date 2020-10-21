0fbc853cf814463d40ae360f30608e5fd4d72f0d
Artem Pakhomov <setnemo@gmail.com>
2020-10-22 02:26:30 +0300

    yellow week 01 ex 07

--- src/cpp-yellow-belt/week01/07_GetRefStrict.cpp
+++ src/cpp-yellow-belt/week01/07_GetRefStrict.cpp
@@ -3,20 +3,10 @@
 #include <stdexcept>
 #include <map>
 
-using namespace std;
 template <typename Value, typename Key>
 Value& GetRefStrict(map<Key, Value>& items, const Key key) {
   if (items.count(key)) {
     return items[key];
   }
   throw runtime_error("");
-}
-
-int main() {
-
-  map<int, string> m = {{0, "value"}};
-  string& item = GetRefStrict(m, 0);
-  item = "newvalue";
-  cout << m[0] << endl; // выведет newvalue
-  return 0;
 }
