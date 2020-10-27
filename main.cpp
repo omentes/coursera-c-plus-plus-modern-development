2374df2607e52972d561d7ad61713d6f6c2edec0
Artem Pakhomov <setnemo@gmail.com>
2020-10-27 23:02:33 +0200

    coursera week 02 ex 01 in progress

--- main.cpp
+++ main.cpp
@@ -46,9 +46,15 @@ istream& operator >> (istream& is, Query& q) {
     is >> q.bus;
     int stop_count;
     is >> stop_count;
-
+    vector<string>& stops = q.stops;
+    stops.resize(stop_count);
+    for (string& stop : stops) {
+      cin >> stop;
+      stops.push_back(q.bus);
+    }
   } else if (operation_code == "BUSES_FOR_STOP") {
     q.type = QueryType::BusesForStop;
+    cin >> q.stop;
   } else if (operation_code == "STOPS_FOR_BUS") {
     q.type = QueryType::StopsForBus;
   } else if (operation_code == "ALL_BUSES") {
@@ -58,20 +64,30 @@ istream& operator >> (istream& is, Query& q) {
 }
 
 struct BusesForStopResponse {
-  // Наполните полями эту структуру
+  bool empty = true;
+  vector<string> stops = {};
 };
 
 ostream& operator << (ostream& os, const BusesForStopResponse& r) {
-  // Реализуйте эту функцию
+  if (r.empty) {
+    os << "No stop";
+  } else {
+    for (const auto& stop : r.stops) {
+      os << stop << " ";
+    }
+    os << endl;
+  }
   return os;
 }
 
 struct StopsForBusResponse {
-  // Наполните полями эту структуру
+  vector<string> res = {};
 };
 
 ostream& operator << (ostream& os, const StopsForBusResponse& r) {
-  // Реализуйте эту функцию
+  for (const auto& line : r.res) {
+    os << line << endl;
+  }
   return os;
 }
 
@@ -87,15 +103,46 @@ ostream& operator << (ostream& os, const AllBusesResponse& r) {
 class BusManager {
  public:
   void AddBus(const string& bus, const vector<string>& stops) {
-    // Реализуйте этот метод
+    vector<string>& _st = buses_to_stops[bus];
+    _st.resize(stops.size());
+    for (const auto& stop : stops) {
+      stops_to_buses[stop].push_back(bus);
+    }
   }
 
   BusesForStopResponse GetBusesForStop(const string& stop) const {
-    // Реализуйте этот метод
+    BusesForStopResponse result;
+    result.stops = {};
+    if (stops_to_buses.count(stop) != 0) {
+      for (const string& bus : stops_to_buses.at(stop)) {
+        result.stops.push_back(bus);
+      }
+      result.empty = false;
+    }
+    return result;
   }
 
   StopsForBusResponse GetStopsForBus(const string& bus) const {
-    // Реализуйте этот метод
+    StopsForBusResponse r = {};
+    if (buses_to_stops.count(bus) == 0) {
+      r.res.push_back("No bus");
+    } else {
+      for (const string& stop : buses_to_stops[bus]) {
+        string current = "";
+        current += "Stop " + ": ";
+        if (stops_to_buses[stop].size() == 1) {
+          current += "no interchange";
+        } else {
+          for (const string& other_bus : stops_to_buses[stop]) {
+            if (bus != other_bus) {
+              current += other_bus + " ";
+            }
+          }
+        }
+        r.res.push_back(current);
+      }
+    }
+    return r;
   }
 
   AllBusesResponse GetAllBuses() const {
