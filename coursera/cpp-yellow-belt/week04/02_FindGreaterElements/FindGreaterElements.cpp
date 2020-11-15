23e60d675eff94977c58e1b6004423b52ca8eb87
Artem Pakhomov <setnemo@gmail.com>
2020-11-15 18:52:30 +0200

    coursera yellow week 04 ex 02

--- coursera/cpp-yellow-belt/week04/02_FindGreaterElements/FindGreaterElements.cpp
+++ coursera/cpp-yellow-belt/week04/02_FindGreaterElements/FindGreaterElements.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <algorithm>
 #include <set>
-#include "reload.h"
 
 using namespace std;
 
@@ -13,15 +12,4 @@ vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
     return current > border;
   });
   return result;
-}
-
-int main() {
-  for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
-    cout << x << " ";
-  }
-  cout << endl;
-
-  string to_find = "Python";
-  cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
-  return 0;
 }
