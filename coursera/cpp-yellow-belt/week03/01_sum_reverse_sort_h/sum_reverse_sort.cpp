adc9fed888b7ed894aa9720751bf863b7df6212f
Artem Pakhomov <setnemo@gmail.com>
2020-11-15 12:58:09 +0200

    coursera yellow week 03 ex 01

---
+++ coursera/cpp-yellow-belt/week03/01_sum_reverse_sort_h/sum_reverse_sort.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include "sum_reverse_sort.h"
+
+int Sum(int x, int y) {
+  return x + y;
+}
+
+string Reverse(string s) {
+  reverse(s.begin(),s.end());
+  return s;
+}
+
+void Sort(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+}
