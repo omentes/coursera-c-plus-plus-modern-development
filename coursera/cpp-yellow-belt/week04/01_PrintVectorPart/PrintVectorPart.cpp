901e5b8242afe5de578b085d8d079383f6c0cce9
Artem Pakhomov <setnemo@gmail.com>
2020-11-15 18:26:29 +0200

    coursera yellow week 04 ex 01

--- coursera/cpp-yellow-belt/week04/01_PrintVectorPart/PrintVectorPart.cpp
+++ coursera/cpp-yellow-belt/week04/01_PrintVectorPart/PrintVectorPart.cpp
@@ -1,4 +1,26 @@
-//
-// Created by omentes on 15.11.20.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+void PrintVectorPart(const vector<int>& numbers) {
+  auto it = find_if(
+      numbers.begin(),
+      numbers.end(),
+      [](const int& current) {
+        return current < 0;
+      }
+  );
+  bool space = false;
+  if (it > numbers.begin()) {
+    auto i = it;
+    while (i != numbers.begin()) {
+      --i;
+      if (space) {
+        cout << ' ';
+      }
+      cout << *i ;
+      space = true;
+    }
+  }
+}
--- main.cpp
+++ main.cpp
@@ -1,62 +1,39 @@
 #include <iostream>
-#include <map>
-#include <set>
-#include <sstream>
-#include <stdexcept>
-#include <string>
 #include <vector>
-#include <exception>
-#include <iomanip>
-#include "test_runner.h"
-#include "coursera/cpp-yellow-belt/week03/02_phone_number/phone_number.h"
+#include <algorithm>
+#include "reload.h"
+
 using namespace std;
 
-void Test00() {
-  try {
-    PhoneNumber phone("");
-  } catch (invalid_argument& e) {
-    string tmp = e.what();
-    if (tmp != "First character was not a plus") {
-      throw exception();
+void PrintVectorPart(const vector<int>& numbers) {
+  auto it = find_if(
+      numbers.begin(),
+      numbers.end(),
+      [](const int& current) {
+        return current < 0;
+      }
+  );
+  bool space = false;
+  if (it > numbers.begin()) {
+    auto i = it;
+    while (i != numbers.begin()) {
+      --i;
+      if (space) {
+        cout << ' ';
+      }
+      cout << *i ;
+      space = true;
     }
   }
 }
 
-void Test01() {
-  try {
-    PhoneNumber phone("+7-1111");
-  } catch (invalid_argument& e) {
-    string tmp = e.what();
-    if (tmp != "Number was contains min 3 parts") {
-      throw exception();
-    }
-  }
-}
-
-void Test02() {
-  try {
-    PhoneNumber phone("+7-1111----");
-  } catch (invalid_argument& e) {
-    string tmp = e.what();
-    if (tmp != "Local number was contains not empty") {
-      throw exception();
-    }
-  }
-}
-
-void Test03() {
-  PhoneNumber phone("+7-495-111-22-33");
-  AssertEqual(phone.GetInternationalNumber(), "+7-495-111-22-33", "GetInternationalNumber");
-  AssertEqual(phone.GetLocalNumber(), "111-22-33", "GetLocalNumber");
-  AssertEqual(phone.GetCityCode(), "495", "GetCityCode");
-  AssertEqual(phone.GetCountryCode(), "7", "GetCountryCode");
-}
 
 int main() {
-  TestRunner tr;
-  tr.RunTest(Test00, "Test00");
-  tr.RunTest(Test01, "Test01");
-  tr.RunTest(Test02, "Test02");
-  tr.RunTest(Test03, "Test03");
+  PrintVectorPart({6, 1, 8, -5, 4});
+  cout << endl;
+  PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+  cout << endl;
+  PrintVectorPart({6, 1, 8, 5, 4});
+  cout << endl;
   return 0;
 }
